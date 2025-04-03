c24a05b19ec6745aab5e6bd8e7c52fe8a8f77862
reg-dra <eduard.danziger@outlook.com>
2025-04-03 17:19:52 +0200

    Initial

---
+++ DeviceRepoAspNetCore/Services/IAudioDeviceStorage.cs
@@ -0,0 +1,14 @@
+﻿using DeviceRepoAspNetCore.Models;
+
+public interface IAudioDeviceStorage
+{
+    IEnumerable<AudioDevice> GetAll();
+    void Add(AudioDevice device);
+    void Remove(string pnpId, string hostName);
+    void UpdateVolume(string pnpId, string hostName, int volume);
+
+    IEnumerable<AudioDevice> Search(string query);
+    IEnumerable<AudioDevice> SearchByField(string field, string query);
+}
+
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# Audio Device Repository Server / ASP.NET Core 
+
+The backend of Audio Device Repository Server as a ASP.Net Core Server with REST API (C# / MongoDB).
+
+
+## Development environment
+
+### Starting ASP.NET Core Web API Server
+
+- Via Terminal using the following command:
+
+```powershell or bash
+cd DeviceRepoAspNetCore
+dotnet run --launch-profile http
+```
