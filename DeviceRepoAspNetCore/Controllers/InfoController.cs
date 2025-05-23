268e6da3f87f536f5e07ef6190526ed38e5be33e
e-danziger <eduard.danziger@googlemail.com>
2025-05-23 23:49:54 +0200

    Info-API with releaseVersion added

---
+++ DeviceRepoAspNetCore/Controllers/InfoController.cs
@@ -0,0 +1,19 @@
+﻿using DeviceRepoAspNetCore.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeviceRepoAspNetCore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InfoController(CodeVersionProvider versionProvider) : ControllerBase
+    {
+        [HttpGet("version")]
+        public IActionResult GetVersion()
+        {
+            return Ok(new
+            {
+                releaseVersion = versionProvider.CodeVersion
+            });
+        }
+    }
+}
